bb67a1ffe0d754e441511e9a7dfdafa04892513e
Wilovy09 <SrWilovy09@gmail.com>
2023-11-17 01:34:09 -0600

    act 2 .sh

--- vercel_build_staticfiles.sh
+++ vercel_build_staticfiles.sh
@@ -4,8 +4,8 @@ yum install -y python3.11
 
 # Install pip for Python 3.11
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.11 get-pip.py
-
+python get-pip.py
+pip install --upgrade pip
 # Install project requirements
 pip install -r requirements.txt
 
