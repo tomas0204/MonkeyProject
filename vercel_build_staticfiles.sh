75f3f45d4c075a18a13b554d1499981dbb9a7448
Wilovy09 <SrWilovy09@gmail.com>
2023-11-17 01:40:37 -0600

    act 3 sh

--- vercel_build_staticfiles.sh
+++ vercel_build_staticfiles.sh
@@ -1,13 +1,3 @@
-# Install Python 3.11.5
-yum install -y https://centos6.iuscommunity.org/ius-release.rpm
-yum install -y python3.11
-
-# Install pip for Python 3.11
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py
-pip install --upgrade pip
-# Install project requirements
+# build_files.sh
 pip install -r requirements.txt
-
-# Build staticfiles
 python manage.py collectstatic --noinput
