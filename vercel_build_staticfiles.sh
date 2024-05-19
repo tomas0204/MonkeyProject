11954ef050ca5d1598bc73c2cb1df114562afea7
tomas0204 <tomasignacioponce17@gmail.com>
2024-05-19 00:13:22 -0300

    .

--- vercel_build_staticfiles.sh
+++ vercel_build_staticfiles.sh
@@ -8,4 +8,4 @@ pip install -r requirements.txt
 
 # Colecta de staticsFiles en theme/static/
 echo "Colectando StaticFiles..."
-python3.9 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
