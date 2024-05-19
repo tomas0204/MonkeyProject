8166a6246723b8e3267480a779e47266cd011d8e
tomas0204 <tomasignacioponce17@gmail.com>
2024-05-19 00:29:14 -0300

    Add vercel

--- vercel_build_staticfiles.sh
+++ vercel_build_staticfiles.sh
@@ -1,11 +1,25 @@
-# build_files.sh
-echo "Instalando requerimientos..."
+#!/bin/bash
+
+# Verificar si virtualenv está instalado, si no, instalarlo
+if ! command -v virtualenv &> /dev/null
+then
+    echo "virtualenv no se encontró, instalándolo..."
+    pip install virtualenv
+fi
+
+# Crear y activar entorno virtual
+python3 -m venv venv
+source venv/bin/activate
+
+# Instalar pip en el entorno virtual si no está disponible
+if ! command -v pip &> /dev/null
+then
+    echo "pip no se encontró en el entorno virtual, instalándolo..."
+    curl https://bootstrap.pypa.io/get-pip.py | python
+fi
+
+# Instalar dependencias
 pip install -r requirements.txt
 
-# Automatic migrations
-#echo "Iniciando migraciones..."
-#python manage.py makemigrations && python manage.py migrate
-
-# Colecta de staticsFiles en theme/static/
-echo "Colectando StaticFiles..."
+# Recolectar archivos estáticos
 python manage.py collectstatic --noinput --clear
