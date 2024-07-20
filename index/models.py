e9bb8ab7a7ece024b5c168e24c13106154b24fab
tomas0204 <tomasignacioponce17@gmail.com>
2024-07-19 22:16:21 -0300

    Portfolio

--- index/models.py
+++ index/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=255)
+    año = models.IntegerField()
+    director = models.CharField(max_length=255)
+    poster = models.URLField()
+
+    def __str__(self):  
+        return self.titulo
--- index/views.py
+++ index/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from datetime import datetime
 
-def index(request):
-    now = datetime.now()
-    data = { 'now': now }
-    return render(request, 'index.html', data)
+def hello(request):
+    return render(request, 'index.html')
+
+def movie(request):
+    if 'q' in request.GET:
+        query = request.GET['q']
+        resultados = Pelicula.objects.filter(titulo__icontains=query)
+        return render(request, 'result.html', {'resultados': resultados, 'query': query})
+    else:
+        return render(request, 'index.html')
