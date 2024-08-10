dbd9f30d2ed127495ee2b0670eb4c367efbaa209
tomas0204 <tomasignacioponce17@gmail.com>
2024-08-10 18:16:54 -0300

    update 5

--- vercel_build_staticfiles.sh
+++ vercel_build_staticfiles.sh
@@ -1,32 +1,15 @@
-#!/bin/bash
-set -e
-
-# Verificar si virtualenv está instalado, si no, instalarlo
-if ! command -v virtualenv &> /dev/null
-then
-    echo "virtualenv no se encontró, instalándolo..."
-    python3 -m pip install virtualenv
-fi
-
-# Eliminar el directorio venv si ya existe
-if [ -d "venv" ]; then
-    echo "Eliminando directorio venv existente..."
-    rm -rf venv
-fi
-
-# Crear y activar entorno virtual
-python3 -m venv venv
+echo "Creando VENV"
+python3.9 -m venv venv
 source venv/bin/activate
 
-# Instalar pip en el entorno virtual si no está disponible
-if ! command -v pip &> /dev/null
-then
-    echo "pip no se encontró en el entorno virtual, instalándolo..."
-    curl https://bootstrap.pypa.io/get-pip.py | python3
-fi
+# build_files.sh
+echo "Instalando requerimientos..."
+pip3 install -r requirements.txt
 
-# Instalar dependencias
-pip install -r requirements.txt
+# Automatic migrations
+#echo "Iniciando migraciones..."
+#python manage.py makemigrations && python manage.py migrate
 
-# Recolectar archivos estáticos
-python3 manage.py collectstatic --noinput --clear
+# Colecta de staticsFiles en theme/static/
+echo "Colectando StaticFiles..."
+python3.9 manage.py collectstatic --noinput
