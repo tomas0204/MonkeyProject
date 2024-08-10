d4c035edb29c86ed811887fdbc93aa85d04ea8f4
tomas0204 <tomasignacioponce17@gmail.com>
2024-08-10 18:13:23 -0300

    update 4

--- vercel_build_staticfiles.sh
+++ vercel_build_staticfiles.sh
@@ -5,7 +5,7 @@ set -e
 if ! command -v virtualenv &> /dev/null
 then
     echo "virtualenv no se encontró, instalándolo..."
-    python -m pip install virtualenv
+    python3 -m pip install virtualenv
 fi
 
 # Eliminar el directorio venv si ya existe
@@ -15,18 +15,18 @@ if [ -d "venv" ]; then
 fi
 
 # Crear y activar entorno virtual
-python -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 
 # Instalar pip en el entorno virtual si no está disponible
 if ! command -v pip &> /dev/null
 then
     echo "pip no se encontró en el entorno virtual, instalándolo..."
-    curl https://bootstrap.pypa.io/get-pip.py | python
+    curl https://bootstrap.pypa.io/get-pip.py | python3
 fi
 
 # Instalar dependencias
 pip install -r requirements.txt
 
 # Recolectar archivos estáticos
-python manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
